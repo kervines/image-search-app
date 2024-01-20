9d0adedca17c37cea374e7e8e6fe126285f1ee58
kervines <kervinzero@gmail.com>
2024-01-20 18:31:50 -0300

    feat - criado função que cria os elementos da pagina <imagem, button e div.card>

---
+++ js/createElements.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const containerImages = document.querySelector('.container-images');
+export function createCardImage(imgElement) {
+  const div = document.createElement('div');
+  div.classList.add('card-image');
+  div.appendChild(imgElement);
+  containerImages.appendChild(div);
+}
+
+export function createImage(url, alt) {
+  const img = document.createElement('img');
+  img.setAttribute('src', url);
+  img.setAttribute('alt', alt);
+  return img;
+}
+
+export function createButton(callback) {
+  const button = document.createElement('button');
+  button.innerText = 'Show more';
+  button.classList.add('btn');
+  container.appendChild(button);
+
+  button.addEventListener('click', callback);
+}
