acd1ed93fe700cbd21312941113f5b150fad2344
kervines <kervinzero@gmail.com>
2024-01-20 18:29:22 -0300

    arquivo `script.js` usando module e import

--- js/script.js
+++ js/script.js
@@ -1,26 +1,2 @@
-const form = document.querySelector('form');
-const inputSearch = document.querySelector('#search');
-const containerImages = document.querySelector('.container-images');
-
-form.addEventListener('submit', handleSubmit);
-function handleSubmit(event) {
-  event.preventDefault();
-  containerImages.innerHTML = '';
-
-  const unsplashAPI = fetch(
-    `https://api.unsplash.com/search/photos?page=1&query=${inputSearch.value}&client_id=${keyAPI}`
-  )
-    .then((response) => response.json())
-    .then((urls) => {
-      urls.results.forEach((url) => {
-        createImage(url.urls.regular, url.alt_description);
-      });
-    });
-}
-
-function createImage(url, alt) {
-  const img = document.createElement('img');
-  img.setAttribute('src', url);
-  img.setAttribute('alt', alt);
-  containerImages.appendChild(img);
-}
+import initFetch from './unsplashFetch.js';
+initFetch();
